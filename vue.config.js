a2529c98dc1fe2c991fb3071967f589270286144
wangjizhi <wangjizhi@meeruu.com>
2020-04-16 23:05:00 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
 
 module.exports = {
-    lintOnSave: false,
-    outputDir: 'docs',
-    devServer: {},
-    chainWebpack: config => {
-        config.resolve.alias
-            .set('@images', path.resolve(__dirname, './src/images'))
-    }
-};
+  lintOnSave: false,
+  outputDir: 'docs',
+  publicPath: 'vue-h5-template',
+  devServer: {},
+  chainWebpack: config => {
+    config.resolve.alias.set('@images', path.resolve(__dirname, './src/images'))
+  },
+}
