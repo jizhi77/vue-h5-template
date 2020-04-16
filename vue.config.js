d4afaaabe6521b61c18bce413d4f6448194a16c5
wangjizhi <wangjizhi@meeruu.com>
2020-04-16 23:10:20 +0800

    修改 publicPath

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   lintOnSave: false,
   outputDir: 'docs',
-  publicPath: 'vue-h5-template',
+  publicPath: '/vue-h5-template',
   devServer: {},
   chainWebpack: config => {
     config.resolve.alias.set('@images', path.resolve(__dirname, './src/images'))
