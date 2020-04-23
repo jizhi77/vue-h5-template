b37c63a662037337baa240fd55d2c48252023eef
wangjizhi <wangjizhi@meeruu.com>
2020-04-23 11:43:22 +0800

    husky、lint-staged

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,21 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   lintOnSave: false,
-  outputDir: 'docs',
-  publicPath: './',
+  outputDir: "docs",
+  publicPath: "./",
   devServer: {
-    port: 1024,
+    port: 1024
   },
   chainWebpack: config => {
-    config.resolve.alias.set('@views', path.resolve(__dirname, './src/views'))
-    config.resolve.alias.set('@images', path.resolve(__dirname, './src/images'))
-    config.resolve.alias.set('@components', path.resolve(__dirname, './src/components'))
-  },
-}
+    config.resolve.alias.set("@views", path.resolve(__dirname, "./src/views"));
+    config.resolve.alias.set(
+      "@images",
+      path.resolve(__dirname, "./src/images")
+    );
+    config.resolve.alias.set(
+      "@components",
+      path.resolve(__dirname, "./src/components")
+    );
+  }
+};
